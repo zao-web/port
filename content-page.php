f580cd145079c5968ed8a2430b162536994690e6
Liz Karaffa <lizkaraffa@gmail.com>
2014-07-14 13:30:37 -0700

    removed comments and made content a child of featured image

--- content-page.php
+++ content-page.php
@@ -20,7 +20,5 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'port' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+
 </article><!-- #post-## -->
