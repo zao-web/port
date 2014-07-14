79f43672a9405a897122388ebf3982b8cc00ae60
Liz Karaffa <lizkaraffa@gmail.com>
2014-07-14 13:31:09 -0700

    removed unnecessary _s theme code

--- page.php
+++ page.php
@@ -15,21 +15,19 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="featured-image">
+				<?php 
+				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+				  the_post_thumbnail( 'full' );
+				} ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+					<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+				<?php endwhile; // end of the loop. ?>
 
-			<?php endwhile; // end of the loop. ?>
-
+			</div><!-- .featured-image -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
