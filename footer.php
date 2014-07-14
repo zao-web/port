193440bc4afc0d22795b7bff3e0cadc14eae736a
Liz Karaffa <lizkaraffa@gmail.com>
2014-07-14 13:44:01 -0700

    added email to footer and centered styling

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<img id="footer-logo" src="<?php echo get_stylesheet_directory_uri() . '/img/logo2-black.png'; ?>" alt="Port LBC Logo" />
+			<a href="mailto:info@portlbc.com"><p class="email">info@portlbc.com</p></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
