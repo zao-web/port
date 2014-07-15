3286327dcfab4ac695b742a0cfc0a7d84f6eea92
Liz Karaffa <lizkaraffa@gmail.com>
2014-07-15 16:32:59 -0700

    unsure the changes made to these files

--- single.php
+++ single.php
@@ -10,8 +10,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-
 			<?php get_template_part( 'content', 'single' ); ?>
 
 			<?php port_post_nav(); ?>
