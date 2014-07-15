ba54538aa30a00ca40112775272b2a8aaee2ccd5
Liz Karaffa <lizkaraffa@gmail.com>
2014-07-14 21:02:53 -0700

    added WPEC file assets

---
+++ template-news.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template: News Page
+ *
+ * Description: This page displays all the featured blog post and the previous and next blog posts.
+ *
+ * @package port
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php port_paging_nav(); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
