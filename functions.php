a7f520765aa9b2dcedd4f7200640ee03ca794926
Liz Karaffa <lizkaraffa@gmail.com>
2014-07-21 17:48:09 -0700

    enqueued scripts

--- functions.php
+++ functions.php
@@ -99,6 +99,8 @@ function port_scripts() {
 
 	wp_enqueue_script( 'port-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script( 'port-overlay', get_template_directory_uri() . '/js/overlay.js', array(), '', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
