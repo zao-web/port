8ee0b8bad5342a5b927cce20818d6214387cef2e
Justin Sainton <justinsainton@gmail.com>
2014-09-10 13:51:52 -0700

    Fix product overlay

--- js/overlay.js
+++ js/overlay.js
@@ -8,8 +8,15 @@ jQuery( document ).ready( function( $ ) {
 	$("#slider-btn").toggle(function() {
 		$('#page-overlay').css('display','block');
 		$('.menu-main-menu-container').css('display','block');
+		$('#logo').html( portOverlayVars.logo_url );
+		$('#slider-btn').html( portOverlayVars.slider_url );
 	}, function(){
 		$('#page-overlay').css('display','none');
 		$('.menu-main-menu-container').css('display','none');
 	});
+
+	$( '.imagecol' ).hover(
+		function() { $( this ).addClass( 'hover' ); },
+		function() { $( this ).removeClass( 'hover' ); }
+	);
 });
